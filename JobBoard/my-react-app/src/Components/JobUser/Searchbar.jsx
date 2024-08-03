1c60c371b6cdf7406d09600e3f017343babd0583
Mudhasheer S <mudhasheer0786@gmail.com>
2024-08-03 17:46:40 +0530

    update ui of userhome page

--- JobBoard/my-react-app/src/Components/JobUser/Searchbar.jsx
+++ JobBoard/my-react-app/src/Components/JobUser/Searchbar.jsx
@@ -2,12 +2,16 @@ import { useContext, useState } from "react";
 import { Admincontext } from "../../App";
 
 function JobSearchForm() {
-  const { jobName, setjobName, location, setLocation } = useContext(Admincontext);
+  const { setjobName, setLocation } = useContext(Admincontext);
 
   //const [location, setLocation] = useState("");
+  const [title,setTitle] = useState("");
+  const [city,setCity] = useState("");
   const handleinput = (e) => {
     e.preventDefault();
-  };
+    setjobName(title);
+    setLocation(city);
+  }
   return (
     <div className="container mx-auto w-full mt-12">
       <div className="flex space-x-3  mx-24 ">
@@ -22,8 +26,8 @@ function JobSearchForm() {
           <input
             type="text"
             id="jobName"
-            value={jobName}
-            onChange={(e) => setjobName(e.target.value)}
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
             className="block w-full px-4 py-2 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border border-gray-400"
             placeholder="e.g., Software Engineer, Google"
           />
@@ -38,8 +42,8 @@ function JobSearchForm() {
           <input
             type="text"
             id="location"
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
             className="block w-full px-3 py-2 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border  border-gray-400"
             placeholder="e.g., San Francisco, CA, 94103, or remote"
           />
